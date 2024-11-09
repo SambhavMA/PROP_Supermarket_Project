51375fd1d410dcc68e68b6379bc502ffe1b85c47
joan.gomez.catala <joan.gomez.catala@estudiantat.upc.edu>
2024-11-09 17:56:33 +0100

    funcions afegides al controller, segurament s'hagin de moure en sub-controladors i no estan del tot ben difinides (p.e.: addDistribution no demana les coses que necessita)

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.distribution.Distribution;
 import model.product.ProductContainer;
 import model.product.Product;
 import model.product.EnumType;
@@ -14,14 +15,48 @@ public class Controller {
     // methods
     // algorithm
     // distribution
+    public void generateDistribution(Distribution distribution) {
+        distributionContainer.addDistribution(distribution);
+        // System.out.println("Distribution generated: " + distribution.getId());
+    }
+//    public void modifyDistribution(Distribution distribution) {
+//        distributionContainer.addDistribution(distribution);
+//        // System.out.println("Distribution modified: " + distribution.getId());
+//    }
+    public void deleteDistribution(int id) {
+        distributionContainer.deleteDistributionById(id);
+        // System.out.println("Distribution deleted: " + id);
+    }
+    public void checkDistribution(int id) {
+        Distribution distribution = distributionContainer.getDistributionById(id);
+        if (distribution != null) {
+            // System.out.println("Distribution found: " + id);
+        } else {
+            // System.out.println("Distribution not found: " + id);
+        }
+    }
     // product
     public void addProduct(String name, EnumType type) {
         Product product = new Product(name, type);
         productContainer.addProduct(product);
-        System.out.println("Product added: " + name);
+        // System.out.println("Product added: " + name);
     }
-    public Product getProductByName(String name) {
-        return productContainer.getProductByName(name);
+    public void modifyProduct(String name, EnumType type) {
+        Product product = productContainer.getProductByName(name);
+        product.setType(type);
+        // System.out.println("Product modified: " + name);
     }
-    // similarity
+    public void deleteProduct(String name) {
+        productContainer.deleteProductByName(name);
+        // System.out.println("Product deleted: " + name);
+    }
+    public void checkProduct(String name) {
+        Product product = productContainer.getProductByName(name);
+        if (product != null) {
+            // System.out.println("Product found: " + name);
+        } else {
+            // System.out.println("Product not found: " + name);
+        }
+    }
+    // similarity tables
 }
